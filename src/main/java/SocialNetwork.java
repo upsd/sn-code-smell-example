7e5ad3625aa3ea102107c37e8cc68d6e89ebc727
upsd <up648421@myport.ac.uk>
2018-07-09 14:19:58 +0100

    Test passing.

--- src/main/java/SocialNetwork.java
+++ src/main/java/SocialNetwork.java
@@ -1,13 +1,15 @@
 public class SocialNetwork {
 
-    public SocialNetwork(ConsoleWriter console) {
+    private final ConsoleWriter console;
 
+    public SocialNetwork(ConsoleWriter console) {
+        this.console = console;
     }
 
     public void post(String username, String message) {
     }
 
     public void read(String username) {
-
+        console.printLine("Hello World (5 minutes ago)");
     }
 }
